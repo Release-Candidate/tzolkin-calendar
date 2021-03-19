f8226f0e8bfe0ac836cceee15f6d813e6c3422bb
Release-Candidate <rec@gmx.at>
2021-03-19 18:32:59 +0100

    Initial commit

---
+++ src/tzolkin-calendar/__main__.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2021 Roland Csaszar
+#
+# Project:  tzolkin-calendar
+# File:     __main__.py
+# Date:     19.Mar.2021
+###############################################################################
+"""The main entry point of the program, does nothing but call the 'real' main function `main`."""
+
+from __future__ import annotations
+
+
+################################################################################
+def main() -> None:
+    """Main function of the program."""
+
+
+if __name__ == "__main__":
+    main()
